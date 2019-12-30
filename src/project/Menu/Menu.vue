9b30b6955fca2c321145d979358066ddf8baea97
guanyue <wb_guanyue@staff.sina.com.cn>
2019-12-30 11:02:09 +0800

    build echo_ark project

---
+++ src/project/Menu/Menu.vue
@@ -0,0 +1,121 @@
+<template>
+    <div class="layout">
+        <Menu mode="horizontal" theme="dark" active-key="1">
+            <div class="layout-logo">是是是</div>
+            <div class="layout-nav">
+                天气情况
+                <!-- <Menu-item key="1">
+                    <Icon type="ios-navigate"></Icon>
+                    导航一
+                </Menu-item>
+                <Menu-item key="2">
+                    <Icon type="ios-keypad"></Icon>
+                    导航二
+                </Menu-item>
+                <Menu-item key="3">
+                    <Icon type="ios-analytics"></Icon>
+                    导航三
+                </Menu-item>
+                <Menu-item key="4">
+                    <Icon type="ios-paper"></Icon>
+                    导航四
+                </Menu-item> -->
+            </div>
+        </Menu>
+        
+        <div class="layout-content">
+            <Row>
+                <i-col span="5">
+                    <Menu   width="auto"  >
+                        <Submenu  name=""  >
+                            <template slot="title">
+                                <Icon type="ios-navigate"></Icon>
+                                导航一
+                            </template>
+                            <Menu-item  name=''>天气</Menu-item>
+                            <Menu-item    name=''>选项 2</Menu-item>
+                            <Menu-item    name=''>选项 3</Menu-item>
+                        </Submenu>
+                        <Submenu name=''>
+                            <template slot="title">
+                                <Icon type="ios-keypad"></Icon>
+                                导航二
+                            </template>
+                            <Menu-item    name=''>选项 1</Menu-item>
+                            <Menu-item  name=''  >选项 2</Menu-item>
+                        </Submenu>
+                        <Submenu name=''>
+                            <template slot="title">
+                                <Icon type="ios-analytics"></Icon>
+                                导航三
+                            </template>
+                            <Menu-item   name='' >选项 1</Menu-item>
+                            <Menu-item   name=''>选项 2</Menu-item>
+                        </Submenu>
+                    </Menu>
+                </i-col>
+                <i-col span="19">
+                    <div class="layout-content-main">
+                        <weather/>
+                    </div>
+                </i-col>
+            </Row>
+        </div>
+        <div class="layout-copy">
+            2011-2016 &copy; TalkingData
+        </div>
+    </div>
+</template>
+<script>
+import weather from '../views/Weather/weather'
+    export default {
+        components: {
+            weather
+        }
+    }
+</script>
+
+<style scoped>
+    .layout{
+        border: 1px solid #d7dde4;
+        background: #f5f7f9;
+    }
+    .layout-logo{
+        width: 100px;
+        height: 30px;
+        background: #5b6270;
+        border-radius: 3px;
+        float: left;
+        position: relative;
+        top: 15px;
+        left: 20px;
+    }
+    .layout-nav{
+        /* width: 420px; */
+        margin: 0 auto;
+    }
+    .layout-assistant{
+        width: 300px;
+        margin: 0 auto;
+        height: inherit;
+    }
+    .layout-breadcrumb{
+        padding: 10px 15px 0;
+    }
+    .layout-content{
+        min-height: 200px;
+        /* height: 100%; */
+        margin: 15px;
+        overflow: hidden;
+        background: #fff;
+        border-radius: 4px;
+    }
+    .layout-content-main{
+        padding: 10px;
+    }
+    .layout-copy{
+        text-align: center;
+        padding: 10px 0 20px;
+        color: #9ea7b4;
+    }
+</style>
---
+++ src/project/views/Weather/weather.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+      <Button type="info" @click="getWeather">获取天气</Button>
+         <pre style="background-color:rgba(0,0,0,.8);color:white;border-radius:3px;max-height:400px;overflow:auto">
+{{weather}}
+        </pre>
+  </div>
+</template>
+
+<script>
+export default {
+    data () {
+        return {
+            weather: {}
+        }
+    },
+    methods: {
+        getWeather () {
+            fetch('https://sapi.k780.com/?app=weather.realtime&weaid=1&ag=today&appkey=47786&sign=bcd5d8c75ea1d1d42a5d8c826438f121&format=json')
+            .then(Response => {
+                if (Response) {
+                    return Response.json()
+                }
+            })
+            .then(json => {
+                this.weather = json.result
+
+            })
+            .catch(error => {
+                console.error(error)
+            })
+            .finally(() => {
+
+            })
+        }
+    },
+    created () {
+     this.getWeather()
+    }
+}
+</script>
+
+<style>
+
+</style>
